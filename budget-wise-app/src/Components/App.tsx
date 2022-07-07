6ee3fa9fbe435b573486ca0be87bbf0093f9c4d6
dbaranov7658 <dima.baranov.2016@inbox.ru>
2022-07-06 18:37:27 -0700

    first commit

---
+++ budget-wise-app/src/Components/App.tsx
@@ -0,0 +1,15 @@
+
+import '../Css/App.css';
+import {Button} from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>
+          test
+      </Button>
+    </div>
+  );
+}
+
+export default App;
