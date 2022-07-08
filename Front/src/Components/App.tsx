687bfabe20af3de3d23c36c7f56bf75a489c2bdb
rsehmbi <rsehmbi@sfu.ca>
2022-07-08 14:36:53 -0700

    feature: adds budget card component

--- Front/src/Components/App.tsx
+++ Front/src/Components/App.tsx
@@ -1,6 +1,8 @@
 
 import '../Css/App.css';
 import {Button} from "antd";
+import BudgetCard from './BudgetCard.tsx';
+import React from 'react';
 
 function App() {
 
@@ -19,9 +21,8 @@ function App() {
 
   return (
     <div className="App">
-      <Button onClick={() => {test()}}>
-          test
-      </Button>
+      <Button onClick={() => {test()}}>test</Button>
+      <BudgetCard cardTitle="Grocery" amount={200} maxAmount={ 1000}></BudgetCard>
     </div>
   );
 }
---
+++ Front/src/Constants/Colors.tsx
@@ -0,0 +1,4 @@
+// Card colors
+export var blueColor = "blue-6"
+export var redColor =  "red-6"
+export var yellowColor = "yellow-6"
