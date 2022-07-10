730904b4f2941d8ad0dbd219f2efbda3acf4269f
Abhay Jolly <abhayjolly18@gmail.com>
2022-07-10 15:49:34 -0700

    Added initial table with columns and displayed it

--- Front/src/Components/BudgetLog.tsx
+++ Front/src/Components/BudgetLog.tsx
@@ -1,5 +1,34 @@
-import React from 'react'
+import React from 'react';
+import {Table} from 'antd';
 
-export default function BudgetTable(){
-    return(<div></div>);
+// Columns to display in logs
+const columns = [
+    {
+        title: 'User ID',
+        dataIndex: 'userId',
+        key: 'userId',
+    },
+    {
+        title: 'Budget Name',
+        dataIndex: 'budgetName',
+        key: 'budgetName',
+    },
+    {
+        title: 'Amount',
+        dataIndex: 'amount',
+        key: 'amount',
+    },
+    {
+        title: 'Max Amount',
+        dataIndex: 'maxAmount',
+        key: 'maxAmount',
+    }
+]
+
+// Table to display
+function BudgetTable(){
+    return(
+    <Table columns={columns}></Table>);
 }
+
+export default BudgetTable;
