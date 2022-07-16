519d00de0e2c4e890971e84a8fe7d03daaba47a1
Abhay Jolly <abhayjolly18@gmail.com>
2022-07-16 00:45:56 -0700

    Merging changes backend

--- Back/Routes/allRoutes.js
+++ Back/Routes/allRoutes.js
@@ -3,7 +3,16 @@ const _r = express.Router()
 const jwt = require("jsonwebtoken");
 
 const { login , isUserAuth, signUp} = require('../Controllers/login')
-const { getbudgetList, getBudgetAggregate, addbudget, addExpense, updatebudget, getBudgetNames, getBudgetLogs, getNameLogs, deleteBudget, deleteAllBudgets } =  require('../Controllers/budget')
+const { getbudgetList, 
+    getBudgetAggregate, 
+    addbudget, addExpense, 
+    updatebudget, 
+    getBudgetNames, 
+    getBudgetLogs, 
+    getNameLogs, 
+    deleteBudget, 
+    deleteAllBudgets, 
+    updateMaxAmount } =  require('../Controllers/budget')
 
 const verifyJWT = (req, res, next) => {
     const token = req.headers["x-access-token"]
@@ -39,6 +48,7 @@ _r.put('/updatebudget', verifyJWT, updatebudget)
 _r.get('/getBudgetNames',verifyJWT, getBudgetNames)
 _r.delete('/deleteBudget', verifyJWT, deleteBudget)
 _r.delete('/deleteAllBudgets', verifyJWT, deleteAllBudgets)
+_r.put('/updateMaxAmount', verifyJWT, updateMaxAmount)
 _r.get('/getbudgetlogs', verifyJWT, getBudgetLogs)
 _r.get('/getbudgetlogs/:name', verifyJWT, getNameLogs)
 
