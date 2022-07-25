03599f8138dc613c723353f7b7a5a8ffae57be77
dbaranov7658 <dima.baranov.2016@inbox.ru>
2022-07-24 17:32:07 -0700

    finalize pie Chart

--- Front/src/Components/Analytics.tsx
+++ Front/src/Components/Analytics.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import {Table, Select, Button, Space, PageHeader, message, Skeleton, Col, Row} from 'antd';
+import {Table, Select, Button, Space, PageHeader, message, Skeleton, Col, Row, DatePicker, Tooltip} from 'antd';
 import { useState, useEffect } from 'react';
 // @ts-ignore
 import {getBudgetLogs, getBudgetNames, getBudNameLogs } from '../Services/BudgetServices.ts';
 // @ts-ignore
 import { parseDate, addCurrency} from "../Utils/UtilFunctions.ts";
-import { DeleteFilled, EditFilled } from "@ant-design/icons";
+import { CaretLeftOutlined } from "@ant-design/icons";
 import { Pie } from '@ant-design/plots';
 
 
@@ -14,10 +14,36 @@ function Analytics(){
     const [budgetList, setBudgetList] = React.useState([]);
     const [selectedGraph, setSelectedGraph] = useState("none");
     const [isSkeleton, setSkeleton] = useState(true);
+    const [startDate, setStartDate] = React.useState("");
+    const [endDate, setEndDate] = React.useState("");
+    const [isSpecificPie, setSpecificPie] = React.useState(false);
+    const [specificPieType, setSpecificPieType] = React.useState("");
+    const [budgetLogs, setBudgetLogs] = useState([]);
+
+
+    const { RangePicker } = DatePicker;
 
     useEffect(() => {
         getBudgetListAPICall();
+        getAllBudgetLogs();
+
     }, [])
+
+    function getAllBudgetLogs() {
+        getBudgetLogs().then((response) => {
+            response.json().then((response) => {
+                if (response.isSuccess) {
+                    parseDate(response.res)        // Set date format
+                    addCurrency(response.res)      // Add currency type
+                    setBudgetLogs(response.res)
+                    // console.log("The budget log is" + response.res)
+                } else {
+                    console.log("Failed to get all budget logs: " + response.error)
+                }
+            })
+        })
+    }
+
 
     const getBudgetListAPICall = async () => {
         await fetch('/getBudgetList', {
@@ -43,11 +69,32 @@ function Analytics(){
     }
 
 
-    const DemoPie = () => {
+    const PieChart = (specificType?: string) => {
         let data = []
-        budgetList.forEach((el) => {
-            data.push({type: el.budgetname, value: parseInt(el.amount)})
-        })
+        if (specificType === null){
+                budgetList.forEach((el) => {
+                    data.push({type: el.budgetname, value: parseInt(el.amount)})
+                })
+        }
+        else{
+            if (startDate !== "" && endDate !== ""){
+                budgetLogs.forEach((el) => {
+                    let date = el.date[6] === "-" ?  parseInt(el.date.substring(5,6)) : parseInt(el.date.substring(5,7))
+                    if (el.budgetcategory === specificType && date <= parseInt(endDate) && date >= parseInt(startDate) ) {
+                        data.push({type: el.description, value: parseInt(el.amount.substring(3, el.amount.length))})
+                    }
+
+                })
+            }
+            else {
+                budgetLogs.forEach((el) => {
+                    if (el.budgetcategory === specificType) {
+                        data.push({type: el.description, value: parseInt(el.amount.substring(3, el.amount.length))})
+                    }
+
+                })
+            }
+        }
         const config = {
             appendPadding: 10,
             data,
@@ -64,14 +111,6 @@ function Analytics(){
                     fontSize: 14,
                 },
             },
-            interactions: [
-                {
-                    type: 'element-selected',
-                },
-                {
-                    type: 'element-active',
-                },
-            ],
             statistic: {
                 title: false,
                 content: {
@@ -84,27 +123,71 @@ function Analytics(){
                 },
             },
         };
-        return <Pie {...config} />;
+        return <Pie onReady={(plot) => {
+                         plot.on('plot:click', (evt) => {
+                             if (evt) {
+                                 handlePieClick(evt)
+                             }
+                         });
+                     }}
+                    {...config} />
     };
+
+    const handlePieClick = (event) => {
+        if (event.data !== undefined && event.data.data.type && specificPieType === ""){
+            setSpecificPie(true)
+            setSpecificPieType(event.data.data.type)
+        }
+    }
+
+    const handlePicker = (e) => {
+            setStartDate(e[0]._d.getMonth())
+            setEndDate(e[1]._d.getMonth())
+    }
 
     return(
         <div style={{paddingTop: "60px"}}>
         <Skeleton style={{padding: "50px 50px 50px 50px"}} loading={isSkeleton}>
             <Row style={{width: "100%", justifyContent: "center", alignItems: "center", paddingTop: "30px"}}>
-                Please select what kind of graph you want to see
+                {specificPieType !== "" ?
+                    <Tooltip placement="bottom" title={"Back"}>
+                    <Button type={"link"}  onClick={() => {
+                        setSpecificPieType("")
+                        setSpecificPie(false)
+                        setStartDate("")
+                        setEndDate("")
+                    }}>
+                        <CaretLeftOutlined />
+                    </Button>
+                    </Tooltip>
+                    :
+                    null
+                }
                 <Select defaultValue={selectedGraph} onChange={(e) => {setSelectedGraph(e)}} style={{width: "200px", marginLeft: "30px"}}>
                     <Select.Option value={"none"} >none</Select.Option>
                     <Select.Option value={"Pie Chart"} >Pie chart</Select.Option>
                 </Select>
+                {specificPieType !== "" ?
+                    <RangePicker format="YYYY-MM" allowClear={false} onChange={(e) => {handlePicker(e)}} style={{width: "250px", marginLeft: "30px"}} picker="month" />
+                    :
+                    null
+                }
             </Row>
             <Row>
                 <Col span={6}>
                 </Col>
                 <Col span={12} style={{height: "600px", paddingTop: "50px"}}>
 
-                    { selectedGraph === "Pie Chart" ?  DemoPie()
+                    {selectedGraph === "Pie Chart" && isSpecificPie ? PieChart(specificPieType)
                         :
-                        null}
+                        null
+                    }
+                    {
+                        selectedGraph === "Pie Chart" && !isSpecificPie ? PieChart(null)
+                            :
+                            null
+                    }
+
 
                 </Col>
                 <Col span={6}>
